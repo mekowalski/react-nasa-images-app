db06bec277503a801700e4444309aaf42b226b44
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-09 12:09:40 -0600

    create callback() and console is correct

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import SearchBar from './SearchBar'
 
 class App extends React.Component{
+  onSearchSubmit(term) {
+    console.log(term)
+  }
 
 
   render() {
     return (
       <div className='ui container' style={{ marginTop: '15px' }}>
-      <SearchBar />
+      <SearchBar
+        onSubmit={this.onSearchSubmit}
+      />
       </div>
     )
   }
