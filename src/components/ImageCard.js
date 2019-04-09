f939551af605da342e73d5a639f8c7e4fba0f6fc
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-09 16:44:24 -0600

    create constructor() for React ref

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,19 @@
  import React from 'react';
 
  class ImageCard extends React.Component {
+   constructor(props) {
+     super(props)
+
+     this.imageRef = React.createRef()
+   }
+
    render() {
      const { title, links } = this.props.image
 
      return (
        <div>
         <img
+          ref={this.imageRef}
           alt={title}
           src={links[0].href}
         />
