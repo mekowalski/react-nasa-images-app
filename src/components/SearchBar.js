439c6d5ff73255419b2daca2db6efd1595896556
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-09 11:04:15 -0600

    explain the process of controlled element after refactor

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -25,3 +25,8 @@ export default SearchBar
 
 //onChange: everytime a user types inside the input, the state will update, the term property with the value
 //on input, new prop value set to this.state.term
+//user types in input
+//callback it invoked
+//setState is called/updated with the new value the user typed in searchbar
+//component re-renders the instant the user types something
+//take value of input(this.state.term), assign it to value prop, overwriting whatever text is inside input
