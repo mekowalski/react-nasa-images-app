92159ddc4d24addc5ec9b0dbf69de909e6e3ec95
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-09 15:35:08 -0600

    destructure image and list still displays correctly

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const ImageList = props => {
-  const images = props.images.map((image) => {
-    return <img alt={image.title} key={image.href} src={image.links[0].href} />
+  const images = props.images.map(({ title, href, links }) => {
+    return <img alt={title} key={href} src={links[0].href} />
   })
-  //technically, i can't use an `id` because that isn't a given property
-  //each image does have a unique href though therefor i used that as the value assigned to key prop
 
   return (
     <div>
